cf76206825c8d724d48af30b0c91736f411562f1
Reese M Bowman <reesemb1@gmail.com>
2020-01-06 21:24:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_data = {}
+  
+  data.each do |outside, inside|
+    inside.each do |attributes, names|
+      names.each do |bird|
+        
+        if !new_pigeon_data[bird]
+          new_pigeon_data[bird] = {}
+        end
+        
+        if !new_pigeon_data[bird][outside]
+          new_pigeon_data[bird][outside] = []
+        end
+        
+        if new_pigeon_data[bird][outside] != attributes
+          new_pigeon_data[bird][outside] << attributes.to_s
+        end
+      end
+    end
+  end 
+  new_pigeon_data
+  #binding.pry
 end
