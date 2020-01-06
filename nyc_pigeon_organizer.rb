38b5a2ba0ae300fd557d756b368278e9c055ed40
Reese M Bowman <reesemb1@gmail.com>
2020-01-06 20:32:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_data = {}
+  
+  data.each do |outside, inside|
+    inside.each do |attributes, name|
+      name.reduce do |bird|
+        binding.pry
+      end
+    end
+  end  
 end
